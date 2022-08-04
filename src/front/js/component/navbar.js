d1b9a7b284e5c70c9cac93b98217b156ffb65a09
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-08-04 22:32:24 +0000

    navbar modificado

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,106 +5,113 @@ import { FaUserCircle } from "react-icons/fa";
 export const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-dark bg-dark fixed-top">
-        <div className="container-fluid justify-content-end">
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <Link to="#">
-                <span className="user-icon text-light"></span>
-					<i className="FaUserCircle"></i>
-              </Link>
-            </li>
-            <li>
-              <Link to="/login">
-                <span className="glyphicon glyphicon-log-in"></span> Login
-              </Link>
-            </li>
-          </ul>
-          <button
-            className="navbar-toggler position-absolute top-1 start-0"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div
-            className="offcanvas offcanvas-start text-bg-dark"
-            tabIndex={-1}
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                Menu
-              </h5>
-              <button
-                type="button"
-                className="btn-close btn-close-white"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              />
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="#">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="#">
-                    Link
-                  </Link>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link
-                    className="nav-link dropdown-toggle"
-                    to="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </Link>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <Link className="dropdown-item" to="#">
-                        Action
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" to="#">
-                        Another action
-                      </Link>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" to="#">
-                        Something else here
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <form className="d-flex" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
+      <div className="container-fluid">
+        <nav className="navbar navbar-dark bg-dark fixed-top">
+          <div className="container-fluid justify-content-end">
+            <Link className="navbar-brand navbar-right" to="#">
+              Navbar
+            </Link>
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <Link to="#" >
+                <span className="user-icon text-light" />
+				</Link>
+              </li>
+              <li>
+                <link to="/login" />
+                <span className="glyphicon glyphicon-log-in text-light" /><FaCat />
+              </li>
+            </ul>
+            <button
+              className="navbar-toggler position-absolute top-1 start-0 m-2"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="offcanvas offcanvas-start text-light bg-dark"
+              tabIndex={-1}
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                  Offcanvas
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close btn-close-white"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
                 />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                  <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to="#">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="#">
+                      Link
+                    </Link>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <Link
+                      className="nav-link dropdown-toggle"
+                      to="#"
+                      id="offcanvasNavbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </Link>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="offcanvasNavbarDropdown"
+                    >
+                      <li>
+                        <Link className="dropdown-item" to="#">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="#">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <hr className="dropdown-divider" />
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="#">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+                <form className="d-flex">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button className="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 };
