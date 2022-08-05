c5325d781574df2494034a1ae1de9d43a904009b
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-08-04 21:54:16 -0400

    incorporan views y problemas de compatibilidad

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
-    return (
-        <footer className="bg-dark footer mt-auto py-3 text-center fixed-bottom">
-		<p className="text-light">
-			Made by <a href="https://github.com/4geeksfinal" target={"_blank"}>Better&Beyond</a> for 4GeeksAcademy FT-16, 2022
-		</p>
-	</footer>
-    )
+	return (
+		<footer className="bg-dark footer mt-auto py-3 text-center sticky-bottom">
+			<p className="text-light">
+				Made by <a href="https://github.com/4geeksfinal" target={"_blank"}>Better&Beyond</a> for 4GeeksAcademy FT-16, 2022
+			</p>
+		</footer>
+	)
 }
 
 export default Footer;
